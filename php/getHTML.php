fccf894ff9018f57e0c39916ee55d7aa77047332
HechEff <haroplanet@gmail.com>
2022-01-16 22:52:55 +0900

    Add values for debugging API

--- php/getHTML.php
+++ php/getHTML.php
@@ -1,3 +1,3 @@
 <?php 
     include("common.php");
-    getTargetHTML($_POST['url'], $_POST['useragent_type'] ?? "");
+    getTargetHTML($_POST['url'] ?? $_GET['url'], $_POST['useragent_type'] ?? $_GET['useragent_type']);
