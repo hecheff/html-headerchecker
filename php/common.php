5fab6b52e17a89f130d0e045fc51aaabf1aaae86
hecheff <haroplanet@gmail.com>
2023-08-06 19:35:07 +0900

    Update test for Nesp reader (test 1)

--- php/common.php
+++ php/common.php
@@ -26,8 +26,10 @@
 
         // Set compatible useragent according to given type, otherwise use working default
         if ($useragent_type == "nesp") {
-            curl_setopt($curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (compatible; 008/0.83; http://www.80legs.com/webcrawler.html) Gecko/2008032620');
-            curl_setopt($curl, CURLOPT_REFERER, 'https://www.nespresso.com/jp/ja/');
+            // curl_setopt($curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (compatible; 008/0.83; http://www.80legs.com/webcrawler.html) Gecko/2008032620');
+            // curl_setopt($curl, CURLOPT_REFERER, 'https://www.nespresso.com/jp/ja/');
+            getPageHTML_curl_v2($url);
+            return false;
         } else {
             curl_setopt($curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36');
             curl_setopt($curl, CURLOPT_AUTOREFERER, true);
@@ -43,6 +45,36 @@
         curl_close($curl);
         if ($retcode == 200) {
             return $output;
+        } else {
+            return "[".$retcode."]";
+        }
+    }
+
+    function getPageHTML_curl_v2 ($url) {
+        $curl = curl_init();
+        $user_agent = 'Mozilla/5.0 (compatible; 008/0.83; http://www.80legs.com/spider.html;) Gecko/2008032620';
+
+        curl_setopt_array($curl, [
+            CURLOPT_URL => $url,
+            CURLOPT_USERAGENT => $user_agent, 
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "GET",
+            CURLOPT_POSTFIELDS => "",
+            CURLOPT_COOKIE => "akacd_www.nespresso.com=3868762722~rv%3D13~id%3Df6ea788086299aedc97bd8e5e7e89222; akavpau_general_waiting_room=1691317257~id%3D45536bb682b99ee9185937f1b85282a8; _abck=E4D8718D472FC4139E47AA31A8A250E3~-1~YAAQESYHYO8HQZ%2BJAQAAuWDtyQpb%2BTFfcKKJ%2FSdB8799Znb%2BdcCJpuphDMSCAP8Lbl%2BsDkgkRlVOIPO5vKUbgtHkpiC5Xn6KU4Q0lDXBkosS%2BqxrTE5cBRMDGGqJVGf0%2BaxaGhFU%2BLBlT9gx9llrDAYwloXkBcHsqTGcrSMe8Z6A8r6S43VGbUJKrCChBow7l6m1uOshcmbS43%2Fpz6yKQbv2nhDgVCKwWEwdDVUPPdoVWKa9SlzTo2bxhlhicFwfl8sZGwH1SS7XFVJsV%2BcA4NL90FSYD67hY2P6YObUOmoNvDj9j5psjwpLY7GNS4%2F6jSDznSojfdF9kaGUo6s0JEFxqC7C%2FmDk8DBgE%2BT3paJjDNFVNJeVPitpNuM%3D~-1~-1~1691313458; bm_sz=4935BE4367CC036382B5CBA1FFD29598~YAAQESYHYPIHQZ%2BJAQAAuWDtyRTznyG4bEzdXAKYMNqS%2BWMYhl1ltUMGyLUwHIMY4HYGqPe4HIqcQjx5QCVcK%2BBGP%2BYKEVCcRKFpvv8mhOmR%2FzG%2FyA7pVVS7Pfs3XDjn7CydjRHlIMtIjJD8qB13Pl4y2GKEIYgd6%2FUECzEnGX71GZEmpzmTyznB3mJayQ%2BPpBatcHdkMkOJxLnVuxQugWcMpDkEjJ4ueVm33E1AQp%2BqlnWlbreZLEUW07eI8%2FR0PjwYkd5C3YhKVftO8bQxr6CqZVhB54RWX2Q%2FxRFDGveSjkQYU3E%3D~3486260~3753030; JSESSIONID=2E5DD11D612B18877BB163853E43A853; CKI_LANGUAGE=ja; CKI_MARKET=jp; prod_nc2_ecom_nespresso_com-STICKED-TO=s33",
+        ]);
+
+        $response = curl_exec($curl);
+        curl_close($curl);
+        
+        $retcode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        curl_close($curl);
+
+        if ($retcode == 200) {
+            return $response;
         } else {
             return "[".$retcode."]";
         }
